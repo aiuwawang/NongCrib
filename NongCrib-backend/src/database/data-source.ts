22621ef143e7feba4a0ea5dce3fb22e2df8a238e
Aiuwawang <harigun49@gmail.com>
2026-05-01 06:35:01 +0700

    lupa euy

--- NongCrib-backend/src/database/data-source.ts
+++ NongCrib-backend/src/database/data-source.ts
@@ -1,14 +1,17 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 import { User } from '../modules/users/user.entity'; // Pastikan path entity bener
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions & SeederOptions = {
   type: 'postgres', 
-  host: 'localhost',
-  port: 5432,
-  username: 'aiuwawang', // Username DB lu
-  password: 'Usahaaiuwawang',     // Password DB lu
-  database: 'nongcrib_db',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'nongcrib_db',
   entities: [User], // Masukin semua entity di sini
   migrations: ['dist/database/migrations/*.js'], // Tempat file migrasi setelah di-build
   seeds: ['src/database/seeds/**/*.seeder.ts'],  // Tempat file seeder lu
